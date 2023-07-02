482ae77a1f8a2f14c0b06e3c9436b8a8c865f471
HUMBEF0OL <123amitrana0123@gmail.com>
2023-07-02 13:27:52 +0530

    completed fetchJson app

--- fetchJson/index.ts
+++ fetchJson/index.ts
@@ -2,6 +2,23 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(response =>{
-    console.log(response.data);
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+axios.get(url).then(response => {
+    const todoData = response.data as Todo;
+    const id = todoData.id;
+    const title = todoData.title;
+    const completed = todoData.completed;
+    logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+    ID: ${id};
+    title: ${title}
+    finished: ${completed}
+`);
+}
