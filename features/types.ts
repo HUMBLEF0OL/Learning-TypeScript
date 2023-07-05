173788b1d04b704197431e39602af3b55a2e65fc
HUMBEF0OL <123amitrana0123@gmail.com>
2023-07-05 12:21:58 +0530

    types in typescript;

---
+++ features/types.ts
@@ -0,0 +1,16 @@
+
+const today = new Date();
+
+const person ={
+    age:20
+};
+
+class Color{
+
+}
+const red = new Color();
+
+/*
+    Type annotation: we define what type of value will a variable will have
+    Type inference: typescript figure out what type of value will a variable will have
+*/
