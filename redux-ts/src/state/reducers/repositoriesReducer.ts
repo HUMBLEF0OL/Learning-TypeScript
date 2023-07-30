87b36f390a991f4109a85aabe394bb7deddf1366
HUMBEF0OL <123amitrana0123@gmail.com>
2023-07-30 12:42:38 +0530

    reorganized the redux strucutre

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -1,29 +1,27 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
 interface RepositoriesState {
     loading: boolean,
     error: string | null,
     data: Array<String>
 }
 
-interface Action {
-    type: string,
-    payload?: any
-}
-
 const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
     switch (action?.type) {
-        case 'search_repositories':
+        case ActionType.SEARCH_REPOSITORIES:
             return {
                 loading: true,
                 error: null,
                 data: []
             }
-        case 'search_repositories_success':
+        case ActionType.SEARCH_REPOSITORIES_SUCCESS:
             return {
                 loading: false,
                 error: null,
                 data: action?.payload
             }
-        case 'search_repositories_error':
+        case ActionType.SEARCH_REPOSITORIES_ERROR:
             return {
                 loading: false,
                 error: action?.payload,
